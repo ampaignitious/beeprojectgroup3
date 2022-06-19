bc803e785fc54b60769c8fbaf470bc478125c896
unknown <ignitiousampa08@gmail.com>
2022-06-19 19:19:52 +0300

    added postgres database configuration

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ImagecrudController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/create',[ImageCrudController::class,'create']);
+Route::get('/get',[ImageCrudController::class,'get']);
+Route::patch('/edit/{id}',[ImageCrudController::class,'edit']);
+Route::post('/update/{id}',[ImageCrudController::class,'update']);
+Route::delete('/delete/{id}',[ImageCrudController::class,'delete']);
